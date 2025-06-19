e502757c40093179dd93372c7ec11347c0a02fb2
HarshitaRaotole <2024.harshita.raotole@ves.ac.in>
2025-06-19 17:22:58 +0530

    Configure Vercel rewrites 4

--- app/api/index.js
+++
@@ -1,6 +0,0 @@
-// backend/api/index.js
-import app from "../server.js" // Import the Express app instance from your main server.js
-
-// This file serves as the entry point for Vercel's serverless function.
-// It simply exports the Express app instance.
-export default app
